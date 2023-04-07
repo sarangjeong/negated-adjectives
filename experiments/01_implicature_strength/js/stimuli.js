5fab49259d301cd2536b768a169e6b541a09052b
sarangjeong <sarangj@stanford.edu>
2023-04-07 10:46:50 -0700

    fix(stimuli): fix '

--- experiments/01_implicature_strength/js/stimuli.js
+++ experiments/01_implicature_strength/js/stimuli.js
@@ -7,7 +7,7 @@ var all_stims =
     "target": "human",
     "value": "flip",
     "negation": "1",
-    "context": "Jane is taking an Uber to the airport. She wants the driver to drive slowly because she gets carsick. Once she’s gotten off the Uber, she calls her colleague, who she was texting about her issue during the Uber ride. Jane says: ", 
+    "context": "Jane is taking an Uber to the airport. She wants the driver to drive slowly because she gets carsick. Once she's gotten off the Uber, she calls her colleague, who she was texting about her issue during the Uber ride. Jane says: ", 
     "sentence": "His driving wasn't fast.",
     "questionState": "What was the speed of the Uber driver?",
     "questionValue": "What speed did Jane want the Uber driver to have?",
